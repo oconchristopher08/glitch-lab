d5dc5cd156a79633504ffd7991a78b4d833bd381
xstarlight <oconchristopher08@gmail.com>
2026-04-30 18:04:05 +0800

    fix: update pages/Home.tsx

--- glitch-lab/artifacts/glitch-lab/src/pages/Home.tsx
+++ glitch-lab/artifacts/glitch-lab/src/pages/Home.tsx
@@ -1,14 +1,8 @@
-// @ts-ignore
 import Hero from "@/components/Hero";
-// @ts-ignore
 import Experiments from "@/components/Experiments";
-// @ts-ignore
 import Vision from "@/components/Vision";
-// @ts-ignore
 import Modules from "@/components/Modules";
-// @ts-ignore
 import EnterLab from "@/components/EnterLab";
-// @ts-ignore
 import Footer from "@/components/Footer";
 
 export default function Home() {
